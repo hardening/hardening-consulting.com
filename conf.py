3f4a296bc7546baa88dff4f1e06de8561323d98c
David Fort <contact@hardening-consulting.com>
2025-09-05 10:21:56 +0200

    Added articles and fixed video tags

--- conf.py
+++ conf.py
@@ -843,7 +843,7 @@ DEPLOY_DRAFTS = False
 # done in the code, hope you don't mind ;-)
 # Note: most Nikola-specific extensions are done via the Nikola plugin system,
 #       with the MarkdownExtension class and should not be added here.
-MARKDOWN_EXTENSIONS = ['fenced_code', 'codehilite'] #video
+MARKDOWN_EXTENSIONS = ['fenced_code', 'codehilite']
 
 # Extra options to pass to the pandoc comand.
 # by default, it's empty, is a list of strings, for example
